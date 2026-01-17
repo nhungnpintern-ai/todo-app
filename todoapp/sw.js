642fa8ff8ff3f7b0e610f62ae5fbc77c296fbb37
nhungintern <nhungnp.intern@vnext.vn>
2026-01-18 00:35:50 +0900

    update sw.js now

--- todoapp/sw.js
+++ todoapp/sw.js
@@ -1,8 +1,22 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker: Installed');
+const CACHE_NAME = "Nhung Daily Planner";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./images/2.png",
+  "./images/1.jpg"
+];
+
+// Install
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  // Giúp app load nhanh hơn
-  e.respondWith(fetch(e.request));
+// Fetch
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
